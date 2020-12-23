2a0b93d036be4fc7e6b0e40f37592aa2e9a18c46
Danny Festor <danny@festor.info>
2020-12-23 14:22:16 +0900

    Configure Express to Use Nunjucks

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index.njk", {
+    title: "My First Nunjucks App",
+    message: "Welcome to Nunjucks Express",
+  });
 });
 
 module.exports = router;
