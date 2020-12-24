7f9f4fe7aaf2beed3ea30a856356216fe01d35e7
Danny Festor <danny@festor.info>
2020-12-24 10:36:59 +0900

    Add Prisma.Io EXAMPLE for Database

--- src/app.ts
+++ src/app.ts
@@ -8,11 +8,17 @@ import cookieParser from "cookie-parser";
 /* Module imports */
 import logger from "morgan";
 import nunjucks from "nunjucks";
+import dotenv from "dotenv";
+
+/* DataBase with Prisma */
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 /* Routes imports */
 import routes from "./routes/";
 
 /* Declare app */
+dotenv.config();
 const app = express();
 
 app.set("view engine", "html");
@@ -28,5 +34,21 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
+
+async function main() {
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true },
+  });
+  console.log(post);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
 
 export default app;
