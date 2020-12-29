667c91b0b78a56d6449b31bd66666ca03a5ae41e
Danny Festor <danny@festor.info>
2020-12-29 12:27:42 +0900

    Inject Colors on SVGs

--- src/controllers/CategoryController.ts
+++ src/controllers/CategoryController.ts
@@ -7,6 +7,7 @@ async function index(req: Request, res: Response) {
   const user = JSON.parse(req.session.user);
   const categories = await prisma.category.findMany({
     where: { userId: user.id },
+    include: { subcategories: true },
   });
   return res.render("auth/category/index.njk", { categories: categories });
 }
@@ -46,6 +47,7 @@ async function edit(req: Request, res: Response) {
     return res.render("back");
   const category = await prisma.category.findUnique({
     where: { id: id },
+    include: { subcategories: true },
   });
   if (!category) return res.redirect("/users/categories");
   return res.render("auth/category/edit.njk", {
---
+++ src/public/javascripts/injectColor.js
@@ -0,0 +1,24 @@
+function injectSVG(event) {
+  const image = event.target;
+  const color = image.getAttribute("color");
+
+  console.log(image, color);
+
+  fetch(image.src)
+    .then((res) => {
+      return res.text();
+    })
+    .then((data) => {
+      const parser = new DOMParser();
+      const svg = parser
+        .parseFromString(data, "image/svg+xml")
+        .querySelector("svg");
+      if (image.id) svg.id = image.id;
+      if (image.className) svg.classList = image.classList;
+      svg.style.fill = color;
+      image.parentNode.replaceChild(svg, image);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
